2a4e19744007e4f92f407f1dec2080b928ada793
Ketan Bhangale <72434174+Ketanbhangale21@users.noreply.github.com>
2024-01-29 15:04:14 +0530

    Update ProtectedRoutes.js

--- ReactDay5/ReactDay5Assignment/frontend/components/ProtectedRoutes.js
+++ ReactDay5/ReactDay5Assignment/frontend/components/ProtectedRoutes.js
@@ -3,25 +3,19 @@ import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
   const navigate = useNavigate();
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const checkUserToken = () => {
     const userToken = sessionStorage.getItem("user-token");
 
     if (!userToken || userToken === "undefined") {
       setIsLoggedIn(false);
       return navigate("/login?returnUrl=" + props.returnUrl);
- 
     }
     setIsLoggedIn(true);
   };
-
   useEffect(() => {
-  
     checkUserToken();
   }, [isLoggedIn]);
-
   return <>{isLoggedIn ? props.children : null}</>;
 };
 
